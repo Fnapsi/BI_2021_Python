9af8aca40d99d81b529359f4e1fc2d6f1af8e67a
fnapsi <mikhail.a.panfilov@gmail.com>
2021-09-29 20:46:49 +0300

    Add possibility to use the converter many times

--- units_conveter.py
+++ units_conveter.py
@@ -5,38 +5,45 @@ def is_number(str):
     except ValueError:
         return False
 
-b = input("Choose your system from F, C, K:")
-while b not in {'F', 'C', 'K'}:
-    b = input("Please, enter some right symbol:")
 
-a = input("Please, enter the temperature value :")
-while not is_number(a):
-    a = input("Please, enter some number:")
-if b == 'K':
-    while not (float(a) >= 0):
-        a = input("Please, enter some positive number, because your temperature is in K:")
-a = float(a)
+flag = True
 
+while flag:
 
+    b = input("Choose your system from F, C, K:")
+    while b not in {'F', 'C', 'K'}:
+        b = input("Please, enter some right symbol:")
 
-c = input("Choose the result system from F, C, K:")
-while c not in {'F', 'C', 'K'}:
-    c = input("Please, enter some right symbol:")
+    a = input("Please, enter the temperature value :")
+    while not is_number(a):
+        a = input("Please, enter some number:")
+    a = float(a)
 
-if b == c:
-    res = a
+    c = input("Choose the result system from F, C, K:")
+    while c not in {'F', 'C', 'K'}:
+        c = input("Please, enter some right symbol:")
 
-else:
-    if (b == 'C') and (c == 'K'):
-        res =a+273
-    elif (b == 'C') and (c == 'F'):
-        res =a*9/5+32
-    elif (b == 'K') and (c == 'C'):
-        res =a-273
-    elif (b == 'K') and (c == 'F'):
-        res = (a - 273)*9/5+32
-    elif (b == 'F') and (c == 'C'):
-        res =(a-32)*5/9
-    elif (b == 'F') and (c == 'K'):
-        res = ( a - 32) * 5 / 9 +273
-print(res,c)
+    if b == c:
+        res = a
+
+    else:
+        if (b == 'C') and (c == 'K'):
+            res = a + 273
+        elif (b == 'C') and (c == 'F'):
+            res = a * 9 / 5 + 32
+        elif (b == 'K') and (c == 'C'):
+            res = a - 273
+        elif (b == 'K') and (c == 'F'):
+            res = (a - 273) * 9 / 5 + 32
+        elif (b == 'F') and (c == 'C'):
+            res = (a - 32) * 5 / 9
+        elif (b == 'F') and (c == 'K'):
+            res = (a - 32) * 5 / 9 + 273
+    print(res, c)
+    f = input("Would you like to continue? print y/n:")
+    while f not in {'y', 'n', 'Y', 'N'}:
+        f = input("Would you like to continue? print y/n:")
+    if f == 'n':
+        flag = False
+print("Thank you, see you later!")
+
