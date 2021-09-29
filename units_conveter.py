d9ca8ad5867fb715117085839e64ec54051114cc
fnapsi <mikhail.a.panfilov@gmail.com>
2021-09-29 20:22:49 +0300

    The first version of temperature converter

---
+++ units_conveter.py
@@ -0,0 +1,39 @@
+def is_number(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+b = input("Choose your system from F, C, K:")
+while b not in {'F', 'C', 'K'}:
+    b = input("Please, enter some right symbol:")
+
+a = input("Please, enter the temperature value :")
+while not is_number(a):
+    a = input("Please, enter some number:")
+a = float(a)
+
+
+
+c = input("Choose the result system from F, C, K:")
+while c not in {'F', 'C', 'K'}:
+    c = input("Please, enter some right symbol:")
+
+if b == c:
+    res = a
+
+else:
+    if (b == 'C') and (c == 'K'):
+        res =a+273
+    elif (b == 'C') and (c == 'F'):
+        res =a*9/5+32
+    elif (b == 'K') and (c == 'C'):
+        res =a-273
+    elif (b == 'K') and (c == 'F'):
+        res = (a - 273)*9/5+32
+    elif (b == 'F') and (c == 'C'):
+        res =(a-32)*5/9
+    elif (b == 'F') and (c == 'K'):
+        res = ( a - 32) * 5 / 9 +273
+print(res,c)
